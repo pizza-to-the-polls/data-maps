57b0c14a547a84b23068f39babecf28e05718aba
Noah Manger <noah.manger@zapier.com>
2019-03-06 22:11:37 -0800

    Adding district data

--- src/main.js
+++ src/main.js
@@ -19,39 +19,35 @@ function parseRow(row) {
   return pieces;
 }
 
-d3.json(sheetsURL)
-  .then(function(response) {
-    response.feed.entry.forEach(d => {
-      const rowContent = d.content.$t.split(', ');
-      const row = parseRow(rowContent);
-      row['id'] = d.title.$t; // Need to add the first column manually
-      districtData.push(row);
-    })
-    // Let's create a table to see what we've got
-    createTable(districtData);
-  });
 
 // Maybe throwaway, but useful to see
 function createTable(data) {
   const keys = Object.keys(data[Object.keys(data)[0]]);
-  const tableHead = document.getElementById('data-table-header');
-  const tableBody = document.getElementById('data-table-body');
-  console.log(tableHead);
-  keys.forEach(k => {
-    const label = document.createElement('th');
-    label.innerHTML = k;
-    tableHead.appendChild(label);
-  });
+  const table = d3
+    .select('body')
+    .append('div')
+    .attr('class', 'table-container')
+    .append('table');
 
-  data.forEach(d => {
-    const row = document.createElement('tr');
-    Object.values(d).forEach(c => {
-        const cell = document.createElement('td');
-        cell.innerHTML = c;
-        row.appendChild(cell);
-    })
-    tableBody.appendChild(row);
-  })
+  const tableHead = table
+    .append('thead')
+    .selectAll('th')
+    .data(keys)
+    .enter()
+    .append('th')
+    .text(d => d);
+
+  const rows = table
+    .append('tbody')
+    .selectAll('tr')
+    .data(data)
+    .enter()
+    .append('tr')
+    .selectAll('td')
+    .data(d => Object.values(d))
+    .enter()
+    .append('td')
+    .text(d => d);
 }
 
 ////// D3 ///////
@@ -66,6 +62,37 @@ const projection = d3
   .translate([svgWidth / 2, svgHeight / 2]);
 
 const geoPathGenerator = d3.geoPath().projection(projection);
+
+async function addDataToFeatures(features) {
+  // Get data from Google Sheet and add it to the geojson data
+  // Returns an enriched set of district data
+  const combinedData = [];
+
+  await d3.json(sheetsURL)
+    .then(function(response) {
+      response.feed.entry.forEach(d => {
+        const rowContent = d.content.$t.split(', ');
+        const row = parseRow(rowContent);
+        row['id'] = Number(d.title.$t); // Need to add the first column manually
+        districtData.push(row);
+      })
+      // Let's create a table to see what we've got
+      createTable(districtData);
+
+      features.forEach(feature => {
+        const data = districtData.find(d => d.id === feature.id);
+        const richDistrict = {...feature, ...data};
+        combinedData.push(richDistrict);
+      })
+    });
+
+    return combinedData;
+}
+
+// Tooltip
+tooltip = d3.select("body").append("div")
+	.attr("class", "tooltip")
+	.style("opacity", 0);
 
 // Load states
 d3
@@ -87,14 +114,38 @@ d3
   .json("/src/us-congress-113.json")
   .then(function(loadedTopoJson) {
     const districtJson = topojson.feature(loadedTopoJson, loadedTopoJson.objects.districts);
-
-    svg
-      .selectAll("path")
-      .data(districtJson.features)
-      .enter()
-      .append("path")
-      .attr("d", geoPathGenerator)
-      .attr("class", "district");
+    addDataToFeatures(districtJson.features)
+      .then(data => {
+        svg
+          .selectAll("path")
+          .data(data)
+          .enter()
+          .append("path")
+          .attr("d", geoPathGenerator)
+          .attr("class", "district")
+          .style("fill", "#229fdd")
+          .style("fill-opacity", d => d.overall)
+          .on('mouseover', d => {
+            tooltip.transition()
+      			.duration(250)
+      			.style("opacity", 1);
+      			tooltip.html(
+        			"<strong>" + d.label + "</strong>" +
+        			"<table><tbody><tr><td class='wide'>Overall:</td><td>" + d.overall + "</td></tr>" +
+        			"<tr><td>Clinton</td><td>" + d.clinton + "</td></tr>" +
+        			"<tr><td>Trump:</td><td>" + d.trump + "</td></tr>" +
+              "<tr><td>Independent:</td><td>" + d.independent + "</td></tr>" +
+              "</tbody></table>"
+      			)
+      			.style("left", (d3.event.pageX + 15) + "px")
+      			.style("top", (d3.event.pageY - 28) + "px");
+      		})
+      		.on("mouseout", function(d) {
+      			tooltip.transition()
+      			.duration(250)
+      			.style("opacity", 0);
+      		});
+      });
 });
 
 // ZOOM ZOOM //
