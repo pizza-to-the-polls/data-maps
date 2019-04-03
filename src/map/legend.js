17d584c2eaa7d0884b79b4e5648945d198a98d06
Scott Duncombe <scott.duncombe@zapier.com>
2019-04-03 10:56:28 -0700

    Fix legend

--- src/map/legend.js
+++ src/map/legend.js
@@ -2,8 +2,11 @@ import * as d3 from "d3";
 import { legendWidth, prefix } from "../constants";
 import { formatAsPercentage } from "../utils";
 
+const colorStops = 11;
+
 const buildLegend = (scale, domain) => {
-  const stops = d3.range(domain[0], domain[1], (domain[1] - domain[0])/11)
+  const domainSize = domain[1] - domain[0];
+  const stops = d3.range(domain[0], domain[1], domainSize/colorStops)
                   .map(i => [i, scale(i)]);
 
   const legendSvg = d3.select(`.${prefix}legend`).select("svg");
@@ -11,23 +14,19 @@ const buildLegend = (scale, domain) => {
   legendSvg.selectAll("*").remove();
   legendSvg.append("g");
 
-  const gradient = legendSvg
-    .append("defs")
-    .append("linearGradient")
-    .attr("id", "gradient")
-    .attr("x1", "0%") // bottom
-    .attr("y1", "0%")
-    .attr("x2", "100%") // to top
-    .attr("y2", "0%")
-    .attr("spreadMethod", "pad");
+  const gradient = legendSvg.append("defs")
+                            .append("linearGradient")
+                            .attr("id", "gradient")
+                            .attr("x1", "0%") // bottom
+                            .attr("y1", "0%")
+                            .attr("x2", "100%") // to top
+                            .attr("y2", "0%")
+                            .attr("spreadMethod", "pad");
 
-  stops.forEach(d => {
-    gradient
-      .append("stop")
-      .attr("offset", d[0])
-      .attr("stop-color", d[1])
-      .attr("stop-opacity", 1);
-  });
+  stops.forEach((d, i) => gradient.append("stop")
+                                  .attr("offset", i/colorStops)
+                                  .attr("stop-color", d[1])
+                                  .attr("stop-opacity", 1));
 
   legendSvg
     .append("rect")
@@ -47,7 +46,7 @@ const buildLegend = (scale, domain) => {
     .attr("fill", "#000")
     .attr("font-size", "12px")
     .attr("y", "25")
-    .attr("x", (d, i) => (i > 0 ? legendWidth - 31 : 0));
+    .attr("x", (_, i) => (i > 0 ? legendWidth - 31 : 0));
 };
 
 export default buildLegend;
