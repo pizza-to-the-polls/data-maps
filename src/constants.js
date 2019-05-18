799e5eeee9bf69d88f8002fd578e0ec618460fba
Noah Manger <noah.manger@zapier.com>
2019-05-18 16:05:43 -0700

    Excluding more keys

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,14 @@
 export const sheetsBaseUrl = "https://spreadsheets.google.com/feeds/list";
-export const excludedKeys = ["id", "fips", "type", "properties", "geometry", "label"];
+export const excludedKeys = [
+  "id",
+  "fips",
+  "type",
+  "properties",
+  "geometry",
+  "label",
+  "scale",
+  "legendLabel"
+];
 export const prefix = "dfp-map__";
 export const rootURL =
   process.env.NODE_ENV === "development"
